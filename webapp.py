0de5bb0ade042bdf360cbe57455743b8ced11e64
Isaiah Gomez <89576103+IsaiahInGithub@users.noreply.github.com>
2023-09-17 20:00:30 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,16 +1,12 @@
 # Import necessary libraries
 import streamlit as st
 import nltk
-import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 st.set_page_config(
     page_title="Sentiment Analysis Tool with VADER",
     page_icon=":speech_balloon:",
 )
-
-uploaded_file = st.sidebar.file_uploader("Choose a CSV or Text file", type=["csv", "txt"])
-df = pd.read_csv(uploaded_file)
 
 # Download the VADER lexicon (only needed once)
 nltk.download('vader_lexicon')
@@ -30,42 +26,23 @@ def analyze_sentiment(text):
     sentiment_scores = sid.polarity_scores(text)
     
     if float(sentiment_scores['compound']) >= 0.5:
-        st.write(df, "Highly Positive")
+        st.write("Highly Positive")
     if float(sentiment_scores['compound']) < 0.5 and float(sentiment_scores['compound']) > 0:
-        st.write(df, "Slightly Positive")
+        st.write("Slightly Positive")
     if float(sentiment_scores['compound']) == 0:
-        st.write(df, "Neutral")
+        st.write("Neutral")
     if float(sentiment_scores['compound']) < 0 and float(sentiment_scores['compound']) >= -0.5:
-        st.write(df, "Slightly Negative")
+        st.write("Slightly Negative")
     if float(sentiment_scores['compound']) < -0.5:
-        st.write(df, "Highly Negative")
+        st.write("Highly Negative")
 
-def analyze_sentiment_df(df, column_name):
-    df['Sentiment'] = df[column_name].apply(analyze_sentiment)
-    return df
-
-    # Create a button to analyze sentiment
-    if st.button("Analyze"):
-        if user_input:
-            sentiment = analyze_sentiment(user_input)
-        else:
-            st.warning("Please enter some text for analysis.")
-    
-if uploaded_file is not None:
-    file_extension = uploaded_file.name.split(".")[-1]
-    if file_extension == "csv":
-        try:
-            st.subheader("Sentiment Analysis for Uploaded CSV File")
-            df = analyze_sentiment_df(df, 'Comments')  # Assuming 'Text' is the column with text data
-            st.write(sentiment + ": " + df)
-        except Exception as e:
-            st.error(f"Error reading the CSV file: {e}")
-    elif file_extension == "txt":
-        file_content = uploaded_file.read()
-        st.subheader("Sentiment Analysis for Uploaded Text File")
-        st.write(file_content)
-        sentiment = analyze_sentiment(file_content)
-        st.write(sentiment + ": " + df)
+# Create a button to analyze sentiment
+if st.button("Analyze"):
+    if user_input:
+        sentiment = analyze_sentiment(user_input)
+        st.write(f"Sentiment: {sentiment}")
+    else:
+        st.warning("Please enter some text for analysis.")
 
 # Add an about section (optional)
 st.sidebar.subheader("About")
