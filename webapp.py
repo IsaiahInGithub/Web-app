3370cf50df93f2b110a1bcb8e355ed098fc48a3e
Isaiah Gomez <89576103+IsaiahInGithub@users.noreply.github.com>
2023-09-17 20:31:19 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -24,7 +24,20 @@ sid = SentimentIntensityAnalyzer()
 # Define a function to analyze sentiment
 def analyze_sentiment(text):
     sentiment_scores = sid.polarity_scores(text)
-    return sentiment_scores
+    compound_score = float(sentiment_scores['compound'])
+    
+    if compound_score >= 0.5:
+        sentiment = "Highly Positive"
+    elif 0.5 > compound_score > 0:
+        sentiment = "Slightly Positive"
+    elif compound_score == 0:
+        sentiment = "Neutral"
+    elif -0.5 <= compound_score < 0:
+        sentiment = "Slightly Negative"
+    else:
+        sentiment = "Highly Negative"
+
+    return sentiment
 
 # Define a function to analyze sentiment for a DataFrame column
 def analyze_sentiment_df(df, column_name):
@@ -37,8 +50,8 @@ sentiment_table = []
 # Create a button to analyze sentiment
 if st.button("Analyze"):
     if user_input:
-        sentiment_scores = analyze_sentiment(user_input)
-        sentiment_table.append(sentiment_scores)
+        sentiment = analyze_sentiment(user_input)
+        sentiment_table.append({'Response': user_input, 'Sentiment': sentiment})
     else:
         st.warning("Please enter some text for analysis.")
 
@@ -52,19 +65,19 @@ if uploaded_file is not None:
             st.write(df)
             # Replace 'YourColumnName' with the actual column name containing text data
             df = analyze_sentiment_df(df, 'Comments')
-            sentiment_table.extend(df['Sentiment'].tolist())
+            sentiment_table.extend(df[['Comments', 'Sentiment']].rename(columns={'Comments': 'Response'}).to_dict('records'))
         except Exception as e:
             st.error(f"Error reading the CSV file: {e}")
     elif file_extension == "txt":
         file_content = uploaded_file.read()
         st.subheader("Sentiment Analysis for Uploaded Text File")
         st.write(file_content)
-        sentiment_scores = analyze_sentiment(file_content)
-        sentiment_table.append(sentiment_scores)
+        sentiment = analyze_sentiment(file_content)
+        sentiment_table.append({'Response': file_content, 'Sentiment': sentiment})
 
-# Display sentiment scores in a table
+# Display sentiment scores in a table with responses
 if sentiment_table:
-    st.subheader("Sentiment Scores")
+    st.subheader("Sentiment Analysis Results")
     sentiment_df = pd.DataFrame(sentiment_table)
     st.write(sentiment_df)
 
