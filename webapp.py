e57bb5f9ddc6f99749caec800ec3391bb252e10e
Isaiah Gomez <89576103+IsaiahInGithub@users.noreply.github.com>
2023-09-18 10:44:05 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,80 +1,85 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from wordcloud import WordCloud 
+from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 # Page config
 st.set_page_config(page_title='Sentiment Analysis App')
 
-# Title 
+# Title
 st.title('Sentiment Analysis App')
 
-# File Upload
-uploaded_file = st.file_uploader('Choose a CSV file', type=['csv']) 
+# File upload
+uploaded_file = st.file_uploader('Choose a CSV file', type='csv')
 
 if uploaded_file:
-   # Load dataframe
-   df = pd.read_csv(uploaded_file)  
-   
-   # Allow column selection
-   column = st.selectbox('Select column', df.columns)
-   texts = df[column]
 
-   # Initialize VADER   
-   analyzer = SentimentIntensityAnalyzer()
-   
-   # Sentiment analysis
-   results = [] 
-   for text in texts:
-      scores = analyzer.polarity_scores(text)
-      sentiment = get_sentiment(scores['compound'])
-      data = {
-         'Response': text,
-         'Sentiment': sentiment,
-         'Compound': scores['compound']
-      }
-      results.append(data)
-      
-   # Display sentiment analysis results   
-   st.dataframe(results) 
-   
-   # Wordcloud
-   wc = generate_wordcloud(texts)
-   st.pyplot(wc)
-   
-   # Parts of speech
-   adjectives, nouns = get_pos(texts)  
-   st.write('Adjectives:', ', '.join(adjectives))
-   st.write('Nouns:', ', '.join(nouns))
-   
+  # Load dataframe
+  df = pd.read_csv(uploaded_file)
+
+  # Column selection
+  column = st.selectbox('Select column', df.columns)
+
+  # Initialize analyzer
+  analyzer = SentimentIntensityAnalyzer()
+
+  # Iterate through column
+  results = []
+  for text in df[column]:
+    text = str(text) # Convert to string
+    scores = analyzer.polarity_scores(text)
+    sentiment = get_sentiment(scores['compound'])
+    
+    data = {
+      'Response': text,
+      'Sentiment': sentiment,
+      'Compound': scores['compound']
+    }
+    results.append(data)
+
+  # Display results
+  st.dataframe(results)
+
+  # Generate wordcloud
+  texts = ' '.join(df[column].astype(str)) 
+  wc = generate_wordcloud(texts)
+  st.pyplot(wc)
+
+  # POS tagging
+  tagged = nltk.pos_tag(word_tokenize(str(texts)))
+  adjectives = get_adjectives(tagged)
+  nouns = get_nouns(tagged)
+
+  st.write('Adjectives:', ', '.join(adjectives))
+  st.write('Nouns:', ', '.join(nouns))
+  
 else:
-   st.info('Upload CSV file')  
+  st.info('Upload a CSV file')
 
-# Helper functions    
+# Helper functions
 def get_sentiment(compound):
-   if compound >= 0.5:
-      return 'Positive'
-   elif compound <= -0.5:
-      return 'Negative'
-   else:
-      return 'Neutral'
+  if compound >= 0.5:
+    return 'Positive'
+  elif compound <= -0.5:
+    return 'Negative'
+  else:
+    return 'Neutral'
 
-def generate_wordcloud(texts):
-   text = ' '.join(texts)
-   stopwords = nltk.corpus.stopwords.words('english')
-   wc = WordCloud(stopwords=stopwords).generate(text)
-   return wc
-   
-def get_pos(texts):
-   tagged = nltk.pos_tag(word_tokenize(texts))  
-   adjectives = [word for word, tag in tagged if tag.startswith('JJ')]
-   nouns = [word for word, tag in tagged if tag.startswith('NN')]
-   return adjectives, nouns
+def generate_wordcloud(text):
+  stopwords = nltk.corpus.stopwords.words('english')
+  wc = WordCloud(width=600, height=400, stopwords=stopwords).generate(text)
+  return wc
 
-# About section 
+def get_adjectives(tagged):
+  return [word for word, tag in tagged if tag.startswith('JJ')]
+
+def get_nouns(tagged):
+  return [word for word, tag in tagged if tag.startswith('NN')]
+  
+# About section
 st.sidebar.header('About')
-st.sidebar.info('This app performs sentiment analysis, generates wordcloud and extracts parts of speech from text columns in a CSV file.')
+st.sidebar.info('Sentiment analysis web app using Streamlit')
