3f7b847aa1ca9caacb817f97e5c2cdf0f3b7e971
Isaiah Gomez <89576103+IsaiahInGithub@users.noreply.github.com>
2023-09-18 12:07:17 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -35,103 +35,104 @@ def classify_sentiment(compound):
         return "Highly Negative"
 
 def generate_wordcloud(text):
-  stopwords_set = set(stopwords.words('english'))
-  wc = WordCloud(width=600, height=400, stopwords=stopwords_set).generate(text)
-  return wc
+    stopwords_set = set(stopwords.words('english'))
+    wc = WordCloud(width=600, height=400, stopwords=stopwords_set).generate(text)
+    return wc
 
 # File upload
 uploaded_file = st.file_uploader('Choose a CSV file', type='csv')
 
 if uploaded_file:
 
-  # Load dataframe
-  df = pd.read_csv(uploaded_file)
+    # Load dataframe
+    df = pd.read_csv(uploaded_file)
 
-  # Column selection
-  column = st.selectbox('Select column', df.columns)
+    # Column selection
+    column = st.selectbox('Select column', df.columns)
 
-  # Initialize analyzer
-  analyzer = SentimentIntensityAnalyzer()
+    # Allow user to add custom stopwords
+    custom_stopwords = st.text_area("Custom Stopwords (comma-separated)", "")
+    custom_stopwords = [word.strip() for word in custom_stopwords.split(',') if word.strip()]
 
-  # Iterate through column
-  results = []
-  for text in df[column]:
-    text = str(text) # Convert to string
-    scores = analyzer.polarity_scores(text)
-    sentiment = classify_sentiment(scores['compound'])
+    # Initialize analyzer
+    analyzer = SentimentIntensityAnalyzer()
+
+    # Iterate through column
+    results = []
+    for text in df[column]:
+        text = str(text) # Convert to string
+        scores = analyzer.polarity_scores(text)
+        sentiment = classify_sentiment(scores['compound'])
+        
+        # POS tagging
+        tagged = nltk.pos_tag(word_tokenize(text))
+        adjectives = [word for word, tag in tagged if tag.startswith('JJ')]
+        verbs = [word for word, tag in tagged if tag.startswith('VB')]
+        proper_nouns = [word for word, tag in tagged if tag == 'NNP' or tag == 'NNPS']
+        common_nouns = [word for word, tag in tagged if tag == 'NN' or tag == 'NNS']
+        
+        data = {
+            'Response': text,
+            'Sentiment': sentiment,
+            'Adjectives': ', '.join(adjectives),
+            'Verbs': ', '.join(verbs),
+            'Proper Nouns': ', '.join(proper_nouns),
+            'Common Nouns': ', '.join(common_nouns),
+            'Compound': scores['compound']
+        }
+        results.append(data)
+
+    # Create a DataFrame from the results
+    result_df = pd.DataFrame(results)
+
+    # Display results
+    st.dataframe(result_df)
+
+    # Generate wordcloud
+    texts = ' '.join(df[column].astype(str))
+    wc = generate_wordcloud(texts)
+
+    # Save WordCloud image to a temporary file with a known extension (e.g., PNG)
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
+        wc.to_file(temp_file.name)
+
+    # Display the saved image
+    st.image(temp_file.name)
     
-    # POS tagging
-    tagged = nltk.pos_tag(word_tokenize(text))
-    adjectives = [word for word, tag in tagged if tag.startswith('JJ')]
-    verbs = [word for word, tag in tagged if tag.startswith('VB')]
-    proper_nouns = [word for word, tag in tagged if tag == 'NNP' or tag == 'NNPS']
-    common_nouns = [word for word, tag in tagged if tag == 'NN' or tag == 'NNS']
-    
-    data = {
-      'Response': text,
-      'Sentiment': sentiment,
-      'Adjectives': ', '.join(adjectives),
-      'Verbs': ', '.join(verbs),
-      'Proper Nouns': ', '.join(proper_nouns),
-      'Common Nouns': ', '.join(common_nouns),
-      'Compound': scores['compound']
-    }
-    results.append(data)
+    # Download the results as a CSV file
+    csv_file = result_df.to_csv(index=False)
+    b64 = base64.b64encode(csv_file.encode()).decode()  # Convert to base64
+    filename = f"{uploaded_file.name.split('.')[0]}_Sentiment_Analysis.csv"
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download CSV</a>'
+    st.markdown(href, unsafe_allow_html=True)
 
-  # Create a DataFrame from the results
-  result_df = pd.DataFrame(results)
+    # Perform K-Means clustering
+    num_clusters = st.slider("Number of Clusters", 2, 10)
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.9, max_features=5000, stop_words=stopwords.words('english') + custom_stopwords)
+    tfidf_matrix = tfidf_vectorizer.fit_transform(result_df['Response'])
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42).fit(tfidf_matrix)
+    result_df['Cluster'] = kmeans.labels_
 
-  # Display results
-  st.dataframe(result_df)
+    # Display clustering results and keywords
+    cluster_counts = result_df.groupby('Cluster').size()
+    st.write("### Clustering Results")
+    st.write(cluster_counts)
 
-  # Generate wordcloud
-  texts = ' '.join(df[column].astype(str))
-  wc = generate_wordcloud(texts)
+    cluster_keywords = {}
+    for cluster_id in range(num_clusters):
+        cluster_text = ' '.join(result_df[result_df['Cluster'] == cluster_id]['Response'])
+        cluster_wc = generate_wordcloud(cluster_text)
+        
+        # Extract keywords from the WordCloud
+        keywords = ', '.join([word for word, _ in cluster_wc.words_.items()][:10])
+        cluster_keywords[cluster_id] = keywords
 
-  # Save WordCloud image to a temporary file with a known extension (e.g., PNG)
-  with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
-      wc.to_file(temp_file.name)
-
-  # Display the saved image
-  st.image(temp_file.name)
-  
-  # Download the results as a CSV file
-  csv_file = result_df.to_csv(index=False)
-  b64 = base64.b64encode(csv_file.encode()).decode()  # Convert to base64
-  filename = f"{uploaded_file.name.split('.')[0]}_Sentiment_Analysis.csv"
-  href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download CSV</a>'
-  st.markdown(href, unsafe_allow_html=True)
-
-  # Allow user to add custom stopwords
-  custom_stopwords = st.text_area("Custom Stopwords (comma-separated)", "")
-  custom_stopwords = [word.strip() for word in custom_stopwords.split(',') if word.strip()]
-
-  # Perform K-Means clustering
-  num_clusters = st.slider("Number of Clusters", 2, 10)
-  tfidf_vectorizer = TfidfVectorizer(max_df=0.9, max_features=5000, stop_words=stopwords.words('english') + custom_stopwords)
-  tfidf_matrix = tfidf_vectorizer.fit_transform(result_df['Response'])
-  kmeans = KMeans(n_clusters=num_clusters, random_state=42).fit(tfidf_matrix)
-  result_df['Cluster'] = kmeans.labels_
-
-  # Display clustering results and keywords
-  cluster_counts = result_df.groupby('Cluster').size()
-  st.write("### Clustering Results")
-  st.write(cluster_counts)
-
-  cluster_keywords = {}
-  for cluster_id in range(num_clusters):
-      cluster_text = ' '.join(result_df[result_df['Cluster'] == cluster_id]['Response'])
-      cluster_wc = generate_wordcloud(cluster_text)
-      cluster_keywords[cluster_id] = ', '.join(cluster_wc.words_.keys()[:10])
-  
-  st.write("### Keywords in Each Cluster")
-  for cluster_id, keywords in cluster_keywords.items():
-      st.write(f"Cluster {cluster_id}: {keywords}")
-
-  st.bar_chart(cluster_counts / len(result_df) * 100)
+    st.write("### Keywords in Each Cluster")
+    for cluster_id, keywords in cluster_keywords.items():
+        st.write(f"Cluster {cluster_id}: {keywords}")
 
 else:
-  st.info('Upload a CSV file')
+    st.info('Upload a CSV file')
 
 # About section
 st.sidebar.header('About')
