67f9f397f43cbc8667a8d7c4874952a0cc457814
Isaiah Gomez <89576103+IsaiahInGithub@users.noreply.github.com>
2023-09-18 10:21:18 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -18,6 +18,7 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
 # Header 
 st.title('Sentiment Analysis App :sunglasses:')
